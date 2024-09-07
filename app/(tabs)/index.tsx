db579592f47f98c849779e8c959dfac101a17751
Yash Jadhav <yashjadhav1411@rocketmail.com>
2024-09-07 14:09:15 +0530

    Load and display images from MediaLIbrary

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,60 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, FlatList,StyleSheet, Text, View } from 'react-native';
 
 import { ScreenContent } from '~/components/ScreenContent';
+import * as MediaLibrary from 'expo-media-library';
+import { useEffect, useState } from 'react';
+import { Image } from 'expo-image';
+
+
+const ScreenWidth = Dimensions.get('window').width;
 
 export default function Home() {
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+
+  const [localAssets, setLocalAssets] = useState<MediaLibrary.Asset[]>([]);
+
+  useEffect(() => {
+    if (permissionResponse?.status !== 'granted') {
+      requestPermission();
+    }
+
+  }, []);
+
+  useEffect(() => {
+
+    if (permissionResponse?.status === 'granted') {
+      loadLocalAssets();
+    }
+
+  }, [permissionResponse]);
+
+  const loadLocalAssets = async () => {
+    const assetsPage = await MediaLibrary.getAssetsAsync()
+    console.log(JSON.stringify(assetsPage, null, 2));
+    setLocalAssets(assetsPage.assets);
+  }
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
+      <Stack.Screen options={{ title: 'Photos' }} />
+      <FlatList
+        data={localAssets}
+        numColumns={4}
+        keyExtractor={(item) => item.id}
+        columnWrapperClassName='gap-[1px]'
+        contentContainerClassName='gap-[1px]'
+        renderItem={({ item }) => (
+          <View style={{}}>
+            <Image
+              source={{ uri: item.uri }}
+              style={{ width: ScreenWidth /4, aspectRatio: 1 }}
+            />
+          </View>
+        )}
+        />
+        <Text>Total assets: {localAssets.length}</Text>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
